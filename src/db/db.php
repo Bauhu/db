e314d142c546ca46feeba1b65b04d208c39f4700
Henry <henry@bauhu.com>
2022-08-17 12:43:57 +0100

    mysqli namespace fix

--- src/db/db.php
+++ src/db/db.php
@@ -12,7 +12,7 @@ class db {
     public int $query_count = 0;
 
     public function __construct($dbhost = 'localhost', $dbuser = 'root', $dbpass = '', $dbname = '', $charset = 'utf8') {
-        $this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+        $this->connection = new \mysqli($dbhost, $dbuser, $dbpass, $dbname);
         if ($this->connection->connect_error) {
             $this->error('Failed to connect to MySQL - ' . $this->connection->connect_error);
         }
